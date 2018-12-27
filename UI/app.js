06a871e9ce599a3c386c8ac0db9dbbec068f8304
Isaac Olagoke <lanre.isaac1@gmail.com>
2018-12-27 22:59:24 +0000

    Adjusted html and css

--- UI/app.js
+++ UI/app.js
@@ -1,12 +1,3 @@
-window.onscroll = function() {myFunction()};
-
-var headnav = document.getElementById("headnav");
-var sticky = headnav.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    headnav.classList.add("sticky")
-  } else {
-    headnav.classList.remove("sticky");
-  }
+module.exports = function() {
+  return 'hello';
 }
